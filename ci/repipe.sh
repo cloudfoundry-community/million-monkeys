5612eea548a148b3a374caa713d22f4bb22573ec
Dr Nic Williams <drnicwilliams@gmail.com>
2017-03-12 17:01:49 -0400

    prettier output for repipe

--- ci/repipe.sh
+++ ci/repipe.sh
@@ -3,9 +3,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-set -x -e
+set -e
 spruce --concourse merge pipeline-base.yml > pipeline.yml
 for release in $(ls releases/*.yml); do
+  echo merging $release
   spruce --concourse merge release.yml $release > release-next.yml
   spruce --concourse merge pipeline.yml release-next.yml > pipeline-with-releases-plus1.yml
   mv pipeline-with-releases-plus1.yml pipeline.yml
