cc64edb0050ce6a659d4cf6dfa36a783b6c6e716
Dr Nic Williams <drnicwilliams@gmail.com>
2017-03-12 11:38:39 -0400

    generated pipeline.yml

--- ci/repipe.sh
+++ ci/repipe.sh
@@ -4,13 +4,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 set -x -e
-spruce --concourse merge pipeline.yml > pipeline-with-releases.yml
+spruce --concourse merge pipeline-base.yml > pipeline.yml
 for release in $(ls releases/*.yml); do
   spruce --concourse merge release.yml $release > release-next.yml
-  spruce --concourse merge pipeline-with-releases.yml release-next.yml > pipeline-with-releases-plus1.yml
-  mv pipeline-with-releases{-plus1,}.yml
+  spruce --concourse merge pipeline.yml release-next.yml > pipeline-with-releases-plus1.yml
+  mv pipeline-with-releases-plus1.yml pipeline.yml
   rm release-next.yml
 done
 
 cd $DIR/..
-fly -t vsphere sp -p $(basename $(pwd)) -c ci/pipeline-with-releases.yml -l <(spruce merge ci/credentials.yml)
+fly -t vsphere sp -p $(basename $(pwd)) -c ci/pipeline.yml -l <(spruce merge ci/credentials.yml)
