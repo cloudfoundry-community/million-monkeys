1d2959b55689023c3c5a0634c438fe698ce7a4e1
Dr Nic Williams <drnicwilliams@gmail.com>
2017-03-12 00:42:01 -0500

    get IP from vault too

--- ci/tasks/safe-get-bosh-creds.sh
+++ ci/tasks/safe-get-bosh-creds.sh
@@ -3,11 +3,13 @@
 safe -k target ci ${VAULT_TARGET:?required}
 echo ${GITHUB_TOKEN:?required} | safe auth github
 
-safe get ${VAULT_PREFIX:?required}
+: ${VAULT_PREFIX:?required}
+
+export BOSH_IP=$(safe get ${VAULT_PREFIX}/env:ip)
 
 cat director_creds/env > <<-YAML
-export BOSH_ENVIRONMENT=https://10.58.111.4:25555
-export BOSH_CA_CERT=$(safe get secret/microbosh/vsphere/prod/certs:rootCA.key)
+export BOSH_ENVIRONMENT=https://$BOSH_IP:25555
+export BOSH_CA_CERT=$(safe get ${VAULT_PREFIX}/certs:rootCA.key)
 export BOSH_CLIENT=admin
-export BOSH_CLIENT_SECRET=$(safe get secret/microbosh/vsphere/prod/users:admin_password)
+export BOSH_CLIENT_SECRET=$(safe get ${VAULT_PREFIX}/users:admin_password)
 YAML
