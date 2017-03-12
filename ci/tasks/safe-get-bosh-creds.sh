4d16c0bde04636c980cc45b73a98f0bf7dff175a
Dr Nic Williams <drnicwilliams@gmail.com>
2017-03-12 01:18:47 -0500

    put root ca into a file

--- ci/tasks/safe-get-bosh-creds.sh
+++ ci/tasks/safe-get-bosh-creds.sh
@@ -6,10 +6,11 @@ echo ${GITHUB_TOKEN:?required} | safe auth github
 : ${VAULT_PREFIX:?required}
 
 export BOSH_IP=$(safe get ${VAULT_PREFIX}/env:ip)
+safe get ${VAULT_PREFIX}/certs:rootCA.key > director_creds/rootCA.key
 
 cat > director_creds/env <<YAML
 export BOSH_ENVIRONMENT=https://$BOSH_IP:25555
-export BOSH_CA_CERT=$(safe get ${VAULT_PREFIX}/certs:rootCA.key)
+export BOSH_CA_CERT=\$(pwd)/director_creds/rootCA.key
 export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=$(safe get ${VAULT_PREFIX}/users:admin_password)
 YAML
