6f8816fae7d7b3496e4b5145ecd7130b7da7c917
Dr Nic Williams <drnicwilliams@gmail.com>
2017-03-12 16:51:21 -0400

    publish yml files summarizing release

--- ci/tasks/export-release.sh
+++ ci/tasks/export-release.sh
@@ -3,6 +3,7 @@
 set -eu
 
 : ${BOSH_DEPLOYMENT:?required}
+: ${BUCKET_URL:?required}
 
 export BOSH_ENVIRONMENT=`bosh-cli int director-state/director-creds.yml --path /internal_ip`
 export BOSH_CA_CERT="$(bosh-cli int director-state/director-creds.yml --path /director_ssl/ca)"
@@ -51,10 +52,33 @@ EOF
 bosh-cli -n -d $BOSH_DEPLOYMENT deploy manifest.yml
 bosh-cli -d $BOSH_DEPLOYMENT export-release $RELEASE_NAME/$RELEASE_VERSION $STEMCELL_OS/$STEMCELL_VERSION
 
-mv *.tgz compiled-release/$( echo *.tgz | sed "s/\.tgz$/-$( date -u +%Y%m%d%H%M%S ).tgz/" )
+# stannis-12.0-ubuntu-trusty-3363.12-20170312-150301-020284203-20170312150310.tgz
+mv *.tgz compiled-release/$RELEASE_NAME-$RELEASE_VERSION-$STEMCELL_OS-$STEMCELL_VERSION.tgz
 sha1sum compiled-release/*.tgz
 
-sha1=$(sha1sum compiled-release/*.tgz | awk '{print $1}')
+RELEASE_SHA1=$(sha1sum compiled-release/*.tgz | awk '{print $1}')
+RELEASE_FILENAME=$(basename compiled-release/*.tgz)
+
+cat > compiled-release/$RELEASE_NAME-latest.spruce.yml <<YAML
+---
+releases:
+- name: $RELEASE_NAME
+  version: $RELEASE_VERSION
+  sha1: $RELEASE_SHA1
+  url: $BUCKET_URL/$RELEASE_FILENAME
+YAML
+cp compiled-release/$RELEASE_NAME-{latest,$RELEASE_VERSION}.spruce.yml
+
+cat > compiled-release/$RELEASE_NAME-latest.patch.yml <<YAML
+- type: replace
+  path: /releases/name=$RELEASE_NAME?
+  value:
+    name: $RELEASE_NAME
+    version: $RELEASE_VERSION
+    sha1: $RELEASE_SHA1
+    url: $BUCKET_URL/$RELEASE_FILENAME
+YAML
+cp compiled-release/$RELEASE_NAME-{latest,$RELEASE_VERSION}.patch.yml
 
 #
 # cleanup
