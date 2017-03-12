0542c93566b10e019a378ce5a6a55f9c2dc0c880
Dr Nic Williams <drnicwilliams@gmail.com>
2017-03-12 09:56:37 -0400

    spruce merge uses vault; not safe

--- ci/tasks/safe-get-bosh-creds.sh
+++ ci/tasks/safe-get-bosh-creds.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-safe -k target ci ${VAULT_TARGET:?required}
-echo ${GITHUB_TOKEN:?required} | safe auth github
-
 : ${VAULT_PREFIX:?required}
+: ${VAULT_ADDR:?required}
 
-export BOSH_IP=$(safe get ${VAULT_PREFIX}/env:ip)
-safe get ${VAULT_PREFIX}/certs:rootCA.key > director_creds/rootCA.key
+# safe -k target ci ${VAULT_TARGET:?required}
+# echo ${GITHUB_TOKEN:?required} | safe auth github
+
+vault auth -method=github token=${GITHUB_TOKEN:?required}
 
 cat > director-creds.spruce.yml <<YAML
 ---
-internal_ip: (( vault $VAULT_PREFIX "/env:ip" ))
-admin_password: (( vault $VAULT_PREFIX "/users:admin_password" ))
+internal_ip: (( vault "$VAULT_PREFIX" "/env:ip" ))
+admin_password: (( vault "$VAULT_PREFIX" "/users:admin_password" ))
 director_ssl:
-  ca: (( vault $VAULT_PREFIX "/certs:rootCA.key" ))
+  ca: (( vault "$VAULT_PREFIX" "/certs:rootCA.key" ))
 YAML
 
 spruce merge director-creds.spruce.yml > director-state/director-creds.yml
