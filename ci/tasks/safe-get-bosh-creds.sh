d9f5860f8ef6d540f723791d8f045e4efff476a0
Dr Nic Williams <drnicwilliams@gmail.com>
2017-03-12 00:47:31 -0500

    fix bash

--- ci/tasks/safe-get-bosh-creds.sh
+++ ci/tasks/safe-get-bosh-creds.sh
@@ -7,7 +7,7 @@ echo ${GITHUB_TOKEN:?required} | safe auth github
 
 export BOSH_IP=$(safe get ${VAULT_PREFIX}/env:ip)
 
-cat director_creds/env > <<-YAML
+cat > director_creds/env <<YAML
 export BOSH_ENVIRONMENT=https://$BOSH_IP:25555
 export BOSH_CA_CERT=$(safe get ${VAULT_PREFIX}/certs:rootCA.key)
 export BOSH_CLIENT=admin
